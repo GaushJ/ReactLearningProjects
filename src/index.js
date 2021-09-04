6061a31857c76ca20583d77e767050234aa82fdc
Gauransh <gauranshjuneja@gmail.com>
2021-09-04 22:49:13 +0530

    Greetings-page-with -react

--- src/index.js
+++ src/index.js
@@ -1,35 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-
-const Time = new Date().getHours();
-let greetingText = '';
-const name = 'Gauransh';
-
-const greetingColor={
-
-};
-if(Time >=1 && Time <=11){
-  greetingText = "Good Morning";
-
-  
-}
-else if(Time >=12 && Time <= 19){
-  greetingText = "Good Afternoon";
-  
-}else {
-  greetingText = "Good Night";
-  greetingColor.color = "yellow";
-}
-  
+import App from './App'
 
 
 
 ReactDOM.render(
 <>
-<div>
-<h1>Hello {name } , <span style = {greetingColor}>{greetingText}</span></h1>
-</div>
+<App />
+
 </>,
 document.getElementById('root')
 );
