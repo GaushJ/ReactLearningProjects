7c2b2873e622771127eca3815e4b32b322c37afe
Gauransh <gauranshjuneja@gmail.com>
2021-09-10 22:28:14 +0530

    Card Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Card from './Card'
+import Header from './Header'
+import Data from './Data'
+
 
 
 
 ReactDOM.render(
 <>
-<App />
+<section className = "section">
+<Header />
 
+<Card 
+image= {Data[0].image}
+category = {Data[0].category}
+title = {Data[0].title}
+link = {Data[0].link}
+/>      
+
+Data.map()
+</section> 
 </>,
 document.getElementById('root')
 );
